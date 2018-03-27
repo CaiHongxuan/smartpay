cddf781fd69292e40ebd5e7357685d9af6e91646
蔡俊贤 <175867739@qq.com>
2018-03-27 23:34:49 +0800

    微信支付相关接口

--- src/WeXinHandler.php
+++ src/WeXinHandler.php
@@ -12,4 +12,52 @@ namespace Hongxuan\Smart;
 class WeXinHandler implements PaymentHandlerInterface
 {
 
+    /**
+     * 手机、电脑网站支付
+     * @return mixed
+     */
+    public function pay()
+    {
+        // TODO: Implement pay() method.
+    }
+
+    /**
+     * 订单查询
+     *
+     * @return mixed
+     */
+    public function tradeQuery()
+    {
+        // TODO: Implement tradeQuery() method.
+    }
+
+    /**
+     * 订单退款
+     *
+     * @return mixed
+     */
+    public function refund()
+    {
+        // TODO: Implement refund() method.
+    }
+
+    /**
+     * 订单退款查询
+     *
+     * @return mixed
+     */
+    public function refundQuery()
+    {
+        // TODO: Implement refundQuery() method.
+    }
+
+    /**
+     * 账单下载
+     *
+     * @return mixed
+     */
+    public function download()
+    {
+        // TODO: Implement download() method.
+    }
 }
